a8252ddf2db0ff2135813aa79e344aee68a41bf5
Naoto Tokoyoda <tkyd89@gmail.com>
2023-01-19 14:18:49 -0800

    updated

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -38,18 +38,18 @@ const Home = () => {
     <div className="home">
       <div className="home__container">
         
-        <img src="https://m.media-amazon.com/images/I/71Kt0pmJIjL._SX3000_.jpg" alt="Home image" className="home__image" />
+        <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="Home image" className="home__image" />
 
         <div className="home__row">
                     <Product
-                        id="12321341"
+                        id="1"
                         title="Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)"
                         price={11.96}
                         rating={5}
                         image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
                     />
                     <Product
-                        id="49538094"
+                        id="2"
                         title="IFB 30 L Convection Microwave Oven (30BRC2, Black, With Starter Kit)"
                         price={239.0}
                         rating={4}
@@ -59,21 +59,21 @@ const Home = () => {
 
                 <div className="home__row">
                     <Product
-                        id="4903850"
+                        id="3"
                         title="All the Light we Cannot See: The Breathtaking World Wide Bestseller Paperback"
                         price={199.99}
                         rating={3}
                         image="https://images-eu.ssl-images-amazon.com/images/I/514kNkerQ0L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
                     />
                     <Product
-                        id="23445930"
+                        id="4"
                         title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
                         price={98.99}
                         rating={5}
                         image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
                     />
                     <Product
-                        id="3254354345"
+                        id="5"
                         title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
                         price={598.99}
                         rating={4}
@@ -83,7 +83,7 @@ const Home = () => {
 
                 <div className="home__row">
                     <Product
-                        id="90829332"
+                        id="6"
                         title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
                         price={1094.98}
                         rating={4}
