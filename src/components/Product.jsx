bd357ab7329b025ba176f9d9d4bda09795413a49
Naoto Tokoyoda <tkyd89@gmail.com>
2023-01-19 14:18:13 -0800

    added rating function

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import "./Product.css";
 
-function Product(props) {
+function Product({id, title, image, price, rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>{props.title}</p>
+                <p>{title}</p>
                 <p className="product__price">
                   <small>$</small>
-                  <strong>{props.price}</strong>
+                  <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                  <p>{props.rating}</p>
+                  {
+                    Array(rating)
+                    .fill()
+                    .map((_,i) => (
+                      <p>⭐</p>
+                    ))
+                  }
                 </div>
             </div>
 
-            <img src={props.image} />
+            <img src={image} />
 
             <button>Add to Basket</button>
         </div>
